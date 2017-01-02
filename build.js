17648bfb7f142882c850b91967da1b378f1ee896
Toby Murray <murray.toby@gmail.com>
2017-01-02 17:10:44 -0500

    Add package.json to encapsulate client and server

---
+++ build.js
@@ -0,0 +1,11 @@
+var fse = require('fs-extra');
+var p = require("path");
+
+const PUBLIC_FOLDER = p.join("min-auth-server", "public");
+const DIST_FOLDER = p.join("min-auth-client", "dist");
+
+// Delete anything that's hanging around in the public folder, as right now we have nothing we want to keep around
+fse.remove(PUBLIC_FOLDER)
+
+// Copy the build artifacts from Angular to Express
+fse.copySync(DIST_FOLDER, PUBLIC_FOLDER)
