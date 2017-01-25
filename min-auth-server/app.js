e5ca3887da63d2904fe1f7614614e22dd3262e02
Toby Murray <murray.toby@gmail.com>
2017-01-25 17:37:28 -0500

    Make formatting consistent

--- min-auth-server/app.js
+++ min-auth-server/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 var express = require('express');
 var path = require('path');
@@ -21,14 +21,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
