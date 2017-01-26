13d06479d70c964070a7c586b7ea294bae9ff6be
Toby Murray <murray.toby@gmail.com>
2017-01-26 12:48:14 -0500

    Add a static header to root element

--- min-auth-client/src/app/app.component.ts
+++ min-auth-client/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   template: `
+  <app-header></app-header>
   <h1>
     {{title}}
   </h1>
