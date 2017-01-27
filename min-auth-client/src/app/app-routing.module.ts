4df9401a7c4ce4f73cee6fe5f8d1a901542bbdc0
Toby Murray <murray.toby@gmail.com>
2017-01-27 12:16:50 -0500

    Add basic routing to each of the components

--- min-auth-client/src/app/app-routing.module.ts
+++ min-auth-client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', component: LandingComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
