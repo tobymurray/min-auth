d053c762e946db45533090168fcc7226844fda4d
Toby Murray <murray.toby@gmail.com>
2017-02-05 10:48:24 -0500

    Use case insensitive text for username and email

--- min-auth-server/migrations/20170202213909_users.js
+++ min-auth-server/migrations/20170202213909_users.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments('user_id');
-    table.string('user_name').unique().notNullable();
-    table.string('user_email').notNullable();
+    table.specificType('user_name', 'citext').unique().notNullable();
+    table.specificType('user_email', 'citext').notNullable();
     table.string('password').notNullable();
     table.string('password_salt').notNullable();
     table.timestamps();
